183368f29f4d421fe7067029552e21cc96d1e13b
Lovish <lovish1234@gmail.com>
2015-10-30 17:31:29 +0000

    corrected earlier appraoch. continue to add the numbers till current  sum is positive instead of stopping at a negetive neumber.

--- array/maths/maxsum.c
+++ array/maths/maxsum.c
@@ -7,6 +7,7 @@
 int maxSubArray(const int* A, int n1) {
     int i,j,sum=0,maxsum=-100000000,negetive;;
     
+    // If the whole array is negetive return the highest value
     for (i=0; i<n1; i++)
     {
         if (A[i]<0)
@@ -23,33 +24,24 @@ int maxSubArray(const int* A, int n1) {
     {
         return maxsum;
     }
-    // if the array is all negetive, select the least negetive number and return it
-    // Line 1 ( Corresponds to arg 1 ) : An integer array. First number is the size S of the array. Then S numbers follow which indicate the elements in the array.
-
-    // take two iterators i and j. whenever i is on a negetive number move it one point
-    // to the right. 
-    maxsum=0;
     
-    for (i=0; i<n1; i++)
+    for(i=0;i<n1;i++)
     {
-        while (A[i] < 0)
+        sum+=A[i];
+        if(sum > 0 )
         {
-            i++;
-        }
-        for(j=i;j<n1;j++)
-        {
-            sum+=A[j];
-            //printf("%d ",sum);
             if ( sum > maxsum )
             {
                 maxsum = sum;
             }
             
         }
-        
-        
-        
+        else
+        {
+            sum=0;
+        }
     }
+  
     
     return maxsum;
 }
