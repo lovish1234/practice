dee8c94fd9363393e8d67d96261cadaaa7379ee1
Lovish <lovish1234@gmail.com>
2015-11-06 18:01:46 +0000

    restructured the merge function

--- array/sorting/mergesort.c
+++ array/sorting/mergesort.c
@@ -3,44 +3,57 @@
 
 void Merge( int *array, int begin, int mid, int end)
 {
-	int i=0,j=mid,k=0;
+	int i=begin,j=mid+1,k=0;
 	int *result = (int *) malloc((end-begin+1) * sizeof(int));
-	while ( i< ((mid-1)-begin+1) && j < (end - mid + 1))
+    
+    
+
+ 	while ( i< ((mid)+1) && j < (end + 1))
 	{
 		if(array[i] <= array[j])
 		{
 			result[k]=array[i];
+//			printf("Replacing result[%d] for array[%d]\n",k,i);
+
 			i++;k++;
 		}
 		else
 		{
 			result[k]=array[j];
+//			printf("Replacing result[%d] for array[%d]\n",k,j);
+
 			j++;k++;
 		}
+	
 	}
-	if(i==((mid-1)-begin+1))
+	if(i==(mid+1))
 	{
-		while(j<(end-mid+1))
+		while(j<(end+1))
 		{
 			result[k]=array[j];
+//			printf("Replacing result[%d] for array[%d]\n",k,j);
 			j++;k++;
 		}
+	
 	}
-	if(j==(end-mid)+1)
+	if(j==(end+1))
 	{
-		while(i< ((mid-1)-begin+1) )
+		while(i< (mid+1) )
 		{
 			result[k]=array[i];
+//			printf("Replacing result[%d] for array[%d]\n",k,i);
+
 			i++;k++;
 		}
 	}
 
 	for(k=0;k<(end-begin+1);k++)
 	{
-		array[begin+k]=result[k];
+		array[k+begin]=result[k];
 	}
-	free(result);
 
+    free(result);
+    return;
 
 }
 			
@@ -49,18 +62,19 @@ void Merge( int *array, int begin, int mid, int end)
 
 void MergeSort( int *array, int begin, int end)
 {
-	printf("%d %d\n",begin,end);
+
     
-//	printf("test");
-	if (((end-begin) + 1) < 2 )
+
+	if (end<=begin)
+// Remember to think simply and avoid killing a bee by bomb		
+//	if (((end-begin) + 1) < 2 )
 	{
 		return;
 	}
 	int mid = begin + (( end - begin )/2);
-//    printf("test");
 
-	MergeSort(array, begin, mid -1);
-	MergeSort(array, mid, end);
+	MergeSort(array, begin, mid );
+	MergeSort(array, mid+1, end);
 	Merge(array, begin, mid, end );
 }
 
@@ -77,7 +91,7 @@ void main()
 		scanf("%d",&array[j]);
 	}
 
-    printf("%d\n",i);
+ //   printf("%d\n\n",i);
    
 	MergeSort(array, 0, (i-1));
     
@@ -85,5 +99,5 @@ void main()
 	{
 		printf("%d ",array[j]);
 	}
-   
+    return 0; 
 }
