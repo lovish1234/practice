7e09eac02f9d2d29839af3f88d6d4fd2385cb71c
Lovish <lovish1234@gmail.com>
2015-11-24 16:57:27 +0000

    insert new intervals into non overlapping intervals( handles corner cases)

--- array/intervals/interval.c
+++ array/intervals/interval.c
@@ -13,98 +13,167 @@
  * newInterval : new Interval to be inserted
  * len : populate the length of returned array of intervals in len
  */
- 
- struct Event 
- {
-     int timeStamp;
-     char beginOrEnd;
- };
- 
- typedef struct Event event;
- 
-int compareFunction(const void* a, const void* b) {
-        event value1 = *((event *)a);
-        event value2 = *((event *)b);
-        if (value1.timeStamp < value2.timeStamp) 
-        {
-            // Return -1 if you want ascending. 1 if you want descending. 
-            return -1;
-        } 
-        else if (value1.timeStamp > value2.timeStamp) 
-        {
-            // Return 1 if you want ascending. -1 if you want descending. 
-            return 1;
-        }
+int max(int a, int b)
+{
+    if(a>b)
+    {
+        return a;
+    }
+    return b;
+}
+
+int min(int a, int b)
+{
+    if(a>b)
+    {
+        return b;
+    }
+    return a;
+}
+int overlap(interval A, interval N)
+{
+    int ans;
+    ans = max(A.start, N.start) <= min(A.end, N.end);
+    if(ans==1)
+    {
+        return 1;
+    }
+    else
+    {
         return 0;
     }
     
-interval* insert(interval *intervals, int sz, interval newInterval, int *len) 
+}
+
+interval superInterval (interval A, interval N)
 {
-    int i, marker=0,noInterval=0;
-    // add event timestamp and its type 
-    event *a = (event*)malloc(sizeof(event)*(2*sz+2));
-    
+    interval ans;
+    ans.start = min(A.start, N.start);
+    ans.end = max(A.end, N.end);
+    return ans;
+}
+
+int checkInterval(interval* A, int sz, interval N)
+{
+    int i;
     for(i=0;i<sz;i++)
     {
-        a[i].timeStamp = intervals[i].start;
-        a[i].beginOrEnd = 'B';
-    }
-    
-    for(i=sz;i<(2*sz);i++)
-    {
-        a[i].timeStamp = intervals[i].end;
-        a[i].beginOrEnd = 'E';
-    }
-    
-    a[2*sz+1].timeStamp = newInterval.start;
-    a[2*sz+1].beginOrEnd = 'B';
-    
-    a[2*sz+2].timeStamp = newInterval.end;
-    a[2*sz+2].beginOrEnd = 'E';
-    
-    qsort(a, (2*sz+2), sizeof(event), compareFunction );
-    
-    // count the number of intervels in result
-    for(i=0;i<(2*sz+2);i++)
-    {
-        if(a[i].beginOrEnd=='B' )
+        if(A[i].end < N.start && A[i+1].start > N.end)
         {
-            if(marker==0)
-            {
-                noInterval++;
-            }
-            marker++;
-        }
-        else
-        {
-            marker--;
+            return i;
         }
     }
-    
-    interval* result = (interval*)malloc(sizeof(interval)*(noInterval));
-    marker=0;
-    noInterval=0;
-    for(i=0;i<(2*sz+2);i++)
+    return -1;
+}
+
+interval* insert(interval *intervals, int sz, interval newInterval, int *len) 
+{
+    // Intervels are already sorted in increasing order.
+    int i,count=0,ibegin;
+    interval superInt = newInterval;
+    for(i=0;i<sz;i++)
     {
-        if(a[i].beginOrEnd=='B')
+        if (overlap(intervals[i], superInt)==1)
         {
-            if(marker==0)
+            if(count==0)
             {
-                result[noInterval].start = a[i].timeStamp;
+                ibegin=i;
             }
-            marker++;
+            count++;
             
+            superInt = superInterval(intervals[i],superInt);
+           // printf("%d %d \n",superInt.start,superInt.end);
+        }
+        
+    }
+    
+    // if size of previous sequence is 0
+    if (sz==0)
+    {
+        *len = 1;
+        interval* result = (interval*)malloc(sizeof(interval)*(*len));
+        result[0]=newInterval;
+        return result;
+    }
+    // if new interval falls before every other interval
+    if( newInterval.end < intervals[0].start)
+    {
+        *len = sz+1;
+        interval* result = (interval*)malloc(sizeof(interval)*(*len));
+        result[0]=newInterval;
+        for(i=1;i<sz+1;i++)
+        {
+            result[i]=intervals[i-1];
+        }
+        return result;
+    }
+    // if new interval falls after every other interval
+    if( newInterval.start > intervals[sz-1].end)
+    {
+        *len = sz+1;
+        interval* result = (interval*)malloc(sizeof(interval)*(*len));
+        result[sz]=newInterval;
+        for(i=0;i<sz;i++)
+        {
+            result[i]=intervals[i];
+        }
+        return result;
+        
+    }
+    // if new interval falls inbetween two intervals 
+    int midIndex = checkInterval( intervals,  sz, newInterval);
+    
+    if (midIndex!=-1)
+    {
+        *len = sz+1;
+        interval* result = (interval*)malloc(sizeof(interval)*(*len));
+       
+        //printf("%d\n",midIndex);
+        for(i=0;i<sz+1;i++)
+        {
+            if(i==(midIndex+1))
+            {
+                result[i]=newInterval;
+                //printf(" New %d %d\n",newInterval.start,newInterval.end);
+            }
+            else if(i<midIndex+1)
+            {
+                result[i]=intervals[i];
+                //printf("%d %d\n",intervals[i].start,intervals[i].end);
+            }
+            else
+            {
+                result[i]=intervals[i-1];
+            }
+        }
+        
+    
+        return result;
+        
+    }
+    
+    
+   // printf("%d %d\n",ibegin,count);
+    
+    *len = sz-count+1;
+    interval* result = (interval*)malloc(sizeof(interval)*(*len));
+    
+    for(i=0;i<(*len);i++)
+    {
+        if(i==ibegin)
+        {
+            result[i] = superInt;
+        }
+        else if(i<ibegin)
+        {
+            result[i] = intervals[i];
         }
         else
         {
-            marker--;
-            if(marker==0)
-            {
-                result[noInterval].end = a[i].timeStamp;
-                noInterval++;
-            }
+            result[i] = intervals[i+count-1];
         }
     }
     
     return result;
+    
 }
