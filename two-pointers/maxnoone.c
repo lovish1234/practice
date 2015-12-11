8801e4502ee7ea4559292d2799fc0072e9158ffc
Lovish <lovish1234@gmail.com>
2015-12-11 18:07:08 +0000

    max number of continues ones given we can flip M bits, O(n), fixed the bug

--- two-pointers/maxnoone.c
+++ two-pointers/maxnoone.c
@@ -8,69 +8,50 @@
 int* maxone(int* A, int n1, int B, int *len1) {
     
     // two index denoting start and end of the series of 1's
-    int i=0,j=0,zeros=0,firstzero=0,sw=0;
-    int length,max_length=0,i_max,j_max;
+    int i=0,j=0,zeros=0;
+    int length=INT_MIN,max_length=0,i_max,j_max;
     while(j<n1)
     {
-        //printf("%d \n",j);
         if(A[j]==1)
         {
             
-            
             length=j-i+1;
-            if(max_length < length)
+            if(length > max_length)
             {
-                printf("1 %d %d %d \n",max_length,i,j);
                 max_length=length;
                 i_max=i;
                 j_max=j;
             }
             j++;
-            
         }
         else
         {
-            
-            
             if(zeros<B)
             {
-                zeros++;
-                if(sw==0)
+                
+                length=j-i+1;
+                if(length > max_length)
                 {
-                    firstzero=j;
-                    printf("%d to %d\n",i,firstzero+1);
-                    sw=1;
+                    max_length=length;
+                    i_max=i;
+                    j_max=j;
                 }
-              
-                    length=j-i+1;
-                    if(max_length < length)
-                    {
-                        printf("0 %d %d %d %d %d %d\n",max_length,i,j,zeros,B,sw);
-                        max_length=length;
-                        i_max=i;
-                        j_max=j;
-                    }
-                    
-                    j++;
-                
-                
+                zeros++;
+                j++;
             }
             else
             {
+                // eliminate one zero from initial part
                 
-                printf("0c %d %d %d %d %d %d\n",max_length,i,j,zeros,B,sw);
-
-                i=firstzero+1;
-                
+                while(A[i]==1)
+                {
+                    i++;
+                }
+                i++;
                 zeros--;
-                
-                
-                sw=0;
             }
-            
         }
     }
-    
    // printf(" %d %d %d \n",max_length,i_max,j_max);
     
     *len1=max_length;
