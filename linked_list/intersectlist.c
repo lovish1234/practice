5cde29ab4801c6ebc157737f906af07bcd9efeec
Lovish <lovish1234@gmail.com>
2015-12-16 12:50:34 +0000

    find the submist in common between two lists, O(n) time and O(1) space

--- linked_list/intersectlist.c
+++ linked_list/intersectlist.c
@@ -20,24 +20,48 @@
  * 
  * @Output head pointer of list.
  */
+
+int listLength(listnode* A)
+{
+    int count=0;
+    listnode* itr=A;
+    while(itr!=NULL)
+    {
+        count++;
+        itr=itr->next;
+    }
+    return count;
+}
 listnode* getIntersectionNode(listnode* A, listnode* B) {
+    
+    listnode *temp;
+    int A_length=listLength(A);
+    int B_length=listLength(B);
+    int init_walk= (A_length>B_length)?(A_length-B_length):(B_length-A_length);
+    if(B_length>A_length)
+    {
+        temp=A;
+        A=B;
+        B=temp;
+    }
     listnode *itrA=A;
     listnode *itrB=B;
-    
+    int i;
+    //printf("%d\n",init_walk);
+    for(i=0;i<init_walk;i++)
+    {
+        itrA=itrA->next;
+    }
     // 0 means the list a sublist of whole list
     while((itrA)!=NULL)
     {
-        itrB=B;
-        while((itrB)!=NULL)
+        //printf("%d %d\n",itrA->val,itrB->val);
+        if((itrA)==(itrB))
         {
-           
-            if((itrA)==(itrB))
-            {
-                return itrA;
-            }
-            
-            itrB=itrB->next;  
+            return itrA;
         }
+            
+        itrB=itrB->next;  
         itrA=itrA->next;
     }
     return NULL;
