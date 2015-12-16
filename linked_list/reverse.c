dac6badb951e9db0323e79ad8a8342989c8ee5cd
Lovish <lovish1234@gmail.com>
2015-12-16 17:59:12 +0000

    reverse a linked list in one traversal

---
+++ linked_list/reverse.c
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ * 
+ * typedef struct ListNode listnode;
+ * 
+ * listnode* listnode_new(int val) {
+ *     listnode* node = (listnode *) malloc(sizeof(listnode));
+ *     node->val = val;
+ *     node->next = NULL;
+ *     return node;
+ * }
+ */
+/**
+ * @input A : Head pointer of linked list 
+ * 
+ * @Output head pointer of list.
+ */
+listnode* reverseList(listnode* A) {
+    listnode* prev=NULL;
+    
+    listnode* current=A;
+    listnode* n;
+    while(current!=NULL)
+    {
+        n=current->next;
+        current->next=prev;
+        prev=current;
+        current=n;
+    }
+    return prev;
+}
