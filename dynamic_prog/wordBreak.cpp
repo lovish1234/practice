c21ddba4400718fce856894de55350c4e762b787
Lovish <lovish1234@gmail.com>
2016-03-24 13:29:54 +0000

    word break based on dictionary, O(n^2) time, O(n2) to O(n) space

--- dynamic_prog/wordBreak.cpp
+++ dynamic_prog/wordBreak.cpp
@@ -1,66 +1,80 @@
-unordered_map <string, int> H;
-
-void init()
-{
-    if(!H.empty())
-    {
-        H.erase(H.begin(),H.end());
-    }
-}
-int wordBreakAlt( string A, vector<string> &B)
-{
-    int stringSize=A.length();
-    int size= B.size();
-    int dictWordSize;
-/*    
-    if(H.find(A)!=H.end())
-    {
-        cout << "here" << " " << A << "\n";
-        return H[A];
-    }
-    */
-    
-    
-    // loop will run n times 
-    for(int i=0;i<size;i++)
-    {
-        //O(sizeof B[i]) operation
-        dictWordSize= B[i].length();
-        //cout << B[i] << " " << A.substr(0, dictWordSize) << "\n";
-        if(B[i].compare(A.substr(0, dictWordSize))==0)
-        {
-            //H[A.substr(0,dictWordSize)]=1;
-            //cout << B[i] << " " << A.substr(0, dictWordSize) << "\n";
-            A = A.substr(dictWordSize);
-            //cout << A << "\n";
-            if(A.empty()==1)
-            {
-                return 1;
-            }
-            else
-            {
-                //H[A]=wordBreakAlt(A, B);
-                return wordBreakAlt(A,B);
-            }
-        }
-    }
-    //H[A]=0;
-    return 0;
-}
-
 int Solution::wordBreak(string A, vector<string> &B) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    init();
-    return wordBreakAlt(A,B);
-    /*
-    for(auto it= H.begin();it!=H.end();++it)
+    int size = A.size();
+    int Bsize = B.size();
+    unordered_map <string,int> dict;
+    for(int i=0;i<Bsize;i++)
     {
-        cout << it->first << " " << it->second << "\n";
+        dict[B[i]]=1;
     }
-    */
     
+    if (A.empty())
+    {
+        return false;
+    }
+    
+    vector<bool> table(A.size(),false);
+    for (int i=size-1;i>=0;i--) 
+    {
+        for (int j=i;j<size;j++) 
+        {
+            string temp(A.begin()+i,A.begin()+j+1);
+            if (dict.find(temp) != dict.end()) 
+            {
+                if (j==size-1 || table[j+1]) 
+                {
+                    table[i] = true;
+                    break;
+                }
+            }
+        }
+    }
+    return table[0];
+    
+    
+    /*
+    vector<vector<bool> > table(size,vector<bool>(size,false));
+    for (int i=0;i<size;i++)
+    {
+        for (int j=i;j<size;j++)
+        {
+            if (dict.find(A.substr(i,j-i+1))!=dict.end())
+            {
+                    table[i][j]=true;
+            }
+        }
+    }
+       
+    for (int i=0;i<size;i++)
+    {
+        for (int j=i;j<size;j++)
+        {
+            for(int k=i;k<j;k++)
+            {
+                if (!table[i][j])
+                {
+                        table[i][j]=table[i][k]&&table[k+1][j];
+                }
+            }
+        }
+    }
+    
+    
+    int i = 0;
+    for (int j=i;j<size;j++)
+    {
+        for(int k=i;k<j;k++)
+        {
+            if (!table[i][j])
+            {
+                table[i][j]=table[i][k]&&table[k+1][j];
+            }
+        }
+    }     
+    return table[0][size-1];
+    */
     
 }
