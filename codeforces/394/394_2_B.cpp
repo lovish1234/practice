f2f3bde2340e93da3a8901719adf169544570718
Lovish <lovish1234@gmail.com>
2017-01-31 23:03:00 +0530

    [comparison][shifting]

---
+++ codeforces/394/394_2_B.cpp
@@ -0,0 +1,219 @@
+#include<cstdio>
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<utility>
+
+#include<cstring>
+#include<string>
+#include<list>
+#include<queue>
+#include<unordered_map>
+#include<unordered_set>
+#include<map>
+#include<set>
+#include<vector>
+#include<tuple>
+#include<bitset>
+
+
+using namespace std;
+
+#define sd(a) scanf("%d",&a)
+#define sd2(a,b) scanf("%d%d",&a,&b)
+#define sd3(a,b,c) scanf("%d%d%d",&a,&b,&c)
+
+#define f(i,n) for(int i=0;i<n;i++)
+#define fab(i,a,b) for(int i=a;i<=b;i++)
+#define fr(n,i) for(int i=(n-1);i>=0;i--)
+#define fabr(i,b,a) for(int i=b;i>=a;i--)
+
+#define debug(a) cout<< a << "\n"
+#define debug2(a,b) cout << a << " " << b << "\n"
+#define debug3(a,b,c) cout << a << " " << b << " " << c << "\n"
+
+#define  mz(array)  memset(array,0,sizeof(array))
+
+#define mt make_tuple
+#define mp make_pair
+#define F first
+#define S second
+#define pb push_back
+#define pf pop_front
+
+
+#define EPS 0.0000001
+
+
+#define ll long long int
+
+typedef pair<int , int> PI;
+typedef vector< int > VI;
+typedef vector < vector < int > > VVI;
+
+
+typedef pair<ll , ll> PL;
+typedef vector< int > VL;
+typedef vector < vector < ll > > VVL;
+
+typedef tuple<int,int,int> TI;
+
+/*
+ * General Purpose Functions 
+ */
+
+template <typename T>  T max2(T x,T y)
+{
+    return  (x>y)?x:y;
+}
+template <typename T> T min2(T x,T y)
+{
+    return  (x<y)?x:y;
+}
+template <typename T>  T min3(T x, T y, T z)
+{
+    return  ((x<y))? ((x<z)?x:z): ((y<z)?y:z);
+}
+template <typename T> T max3 (T x, T y, T z)
+{
+    return   ((x>y))? ((x>z)?x:z): ((y>z)?y:z);
+}
+
+
+template <typename T>  T GCD(T u,T v)
+{
+	if(v==0)
+	{
+		return u;
+	}
+	else
+	{
+		return GCD(v,u%v);
+	}
+}
+
+template <typename T> T LCM(T u, T v)
+{
+	return (u*v)/GCD(u,v);
+}
+
+
+/* Weather a number is prime 
+ */
+int IsPrime(int u)
+{
+	fab(i,2,sqrt(u))
+	{
+		if(!(u%i))
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+/*power of n 
+  */
+ll power(int n, int p)
+{
+	ll ans=1;
+	while(p>0)
+	{
+		ans*=n;
+	}
+	return ans;
+}
+
+/* Depth First Search within a Graph
+ * Requirements : vector <int> visited
+ * 				  vector<int> adj[N]
+ */
+
+/*
+void DFS(int u)
+{
+	if(visited[u]==1)
+	{
+		return;
+	}
+	else
+	{
+		visited[u]=1;
+		f(i,adj[u].size())
+		{
+			if(visited[adj[u][i]]==0)
+			{
+				DFS(adj[u][i]);
+			}
+		}
+	}
+}
+*/
+
+/* Comparator Function
+ */
+bool comparator(pair < ll, pair< ll,ll > > const& a, pair < ll, pair< ll,ll > > const& b)
+{
+	return a.F < b.F;
+}
+
+
+int main()
+{
+	int n,l,temp;
+	cin >> n >> l;
+	vector<int> k,s,diffk,diffs;
+	f(i,n)
+	{
+		cin >> temp;
+		k.pb(temp);
+	}
+	f(i,n)
+	{
+		cin >> temp;
+		s.pb(temp);
+	}
+
+	if(n==1)
+	{
+		cout << "YES";
+		return 0;
+	}
+
+
+	f(i,n-1)
+	{
+		diffk.pb(k[i+1]-k[i]);
+	}
+	diffk.pb(l-k[n-1]+k[0]);
+	f(i,n-1)
+	{
+		diffs.pb(s[i+1]-s[i]);
+	}
+	diffs.pb(l-s[n-1]+s[0]);
+
+	int same=0;
+	f(j,n)
+	{
+		same=0;
+		rotate(diffs.begin(),diffs.begin()+1,diffs.end());
+		f(i,n)
+		{
+			if(diffk[i]!=diffs[i])
+			{
+				break;
+			}
+			else
+			{
+				same++;
+			}
+		}
+		if(same==n)
+		{
+			cout << "YES";
+			return 0;
+		}
+	}
+	cout << "NO";
+	return 0;
+}
