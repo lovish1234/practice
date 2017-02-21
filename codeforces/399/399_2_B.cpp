ccd87a63039e689168e2bfefd5b4e2373446606e
Lovish <lovish1234@gmail.com>
2017-02-21 12:44:08 +0530

    [implementation]

---
+++ codeforces/399/399_2_B.cpp
@@ -0,0 +1,244 @@
+#include<cstdio>
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<utility>
+
+#include<cstring>
+#include<string>
+#include<list>
+#include<queue>
+#include<unordered_map>
+#include<unordered_set>
+#include<map>
+#include<set>
+#include<vector>
+#include<tuple>
+#include<bitset>
+
+
+using namespace std;
+
+#define sd(a) scanf("%d",&a)
+#define sd2(a,b) scanf("%d%d",&a,&b)
+#define sd3(a,b,c) scanf("%d%d%d",&a,&b,&c)
+
+#define f(i,n) for(int i=0;i<n;i++)
+#define fab(i,a,b) for(int i=a;i<=b;i++)
+#define fr(n,i) for(int i=(n-1);i>=0;i--)
+#define fabr(i,b,a) for(int i=b;i>=a;i--)
+
+#define debug(a) cout<< a << "\n"
+#define debug2(a,b) cout << a << " " << b << "\n"
+#define debug3(a,b,c) cout << a << " " << b << " " << c << "\n"
+
+#define  mz(array)  memset(array,0,sizeof(array))
+
+#define mt make_tuple
+#define mp make_pair
+#define F first
+#define S second
+#define pb push_back
+#define pf pop_front
+
+
+#define EPS 0.0000001
+#define MOD 1000000007
+
+#define ll long long int
+
+typedef pair<int , int> PI;
+typedef vector< int > VI;
+typedef vector < vector < int > > VVI;
+
+
+typedef pair<ll , ll> PL;
+typedef vector< ll > VL;
+typedef vector < vector < ll > > VVL;
+
+typedef tuple<int,int,int> TI;
+
+/*
+ * General Purpose Functions 
+ */
+
+template <typename T>  T max2(T x,T y)
+{
+    return  (x>y)?x:y;
+}
+template <typename T> T min2(T x,T y)
+{
+    return  (x<y)?x:y;
+}
+template <typename T>  T min3(T x, T y, T z)
+{
+    return  ((x<y))? ((x<z)?x:z): ((y<z)?y:z);
+}
+template <typename T> T max3 (T x, T y, T z)
+{
+    return   ((x>y))? ((x>z)?x:z): ((y>z)?y:z);
+}
+
+
+template <typename T>  T GCD(T u,T v)
+{
+	if(v==0)
+	{
+		return u;
+	}
+	else
+	{
+		return GCD(v,u%v);
+	}
+}
+
+template <typename T> T LCM(T u, T v)
+{
+	return (u*v)/GCD(u,v);
+}
+
+
+/* Weather a number is prime 
+ */
+int IsPrime(int u)
+{
+	fab(i,2,sqrt(u))
+	{
+		if(!(u%i))
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+/*power of n 
+  */
+template<typename T> T power(T u,T v)
+{
+	T ans=1;
+	while(v>0)
+	{
+		if(v%2==1) 
+			ans=(ans*u);
+		v/=2;
+		u=(u*u);
+	}
+	return ans;
+}
+
+/* Depth First Search within a Graph
+ * Requirements : vector <int> visited
+ * 				  vector<int> adj[N]
+ */
+
+/*
+void DFS(int u)
+{
+	if(visited[u]==1)
+	{
+		return;
+	}
+	else
+	{
+		visited[u]=1;
+		f(i,adj[u].size())
+		{
+			if(visited[adj[u][i]]==0)
+			{
+				DFS(adj[u][i]);
+			}
+		}
+	}
+}
+*/
+
+/* Comparator Function
+ */
+bool comparator(pair < ll, pair< ll,ll > > const& a, pair < ll, pair< ll,ll > > const& b)
+{
+	return a.F < b.F;
+}
+
+ll a[51];
+void calcPower()
+{
+	a[0]=1;
+	fab(i,1,50)
+	{
+		a[i]=(2*a[i-1]);
+	}
+}
+/*
+ll noOfElements(ll n)
+{
+	if(n==0)
+	{
+		return 1;
+	}
+
+	fab(i,0,48)
+	{
+		if((n/a[i])!=0 && (n/a[i+1])==0)
+		{
+			return (a[i+1]-1);
+		}
+	}
+}
+*/		
+
+ll midElementIndex(ll n)
+{
+	if(n==0)
+	{
+		return 1;
+	}
+
+	fab(i,0,49)
+	{
+		if((n/a[i])!=0 && (n/a[i+1])==0)
+		{
+			return (a[i]);
+		}
+	}
+}
+
+ll noOfOnes(ll n,ll l, ll r)
+{
+	ll mid=midElementIndex(n);
+	ll tot=2*mid-1;
+
+	if(l<mid && r>mid)
+	{
+		return noOfOnes(n/2,l,mid-1)+(n%2)+noOfOnes(n/2,1,r-mid);
+	}
+	else if(l<mid && r<mid)
+	{
+		return noOfOnes(n/2,l,r);
+	}
+	else if(l>mid && r>mid)
+	{
+		return noOfOnes(n/2,l-mid,r-mid);
+	}
+	else if(l<mid && r==mid)
+	{
+		return noOfOnes(n/2,l,mid-1)+(n%2);
+	}
+	else if(l==mid && r>mid)
+	{
+		return noOfOnes(n/2,1,r-mid)+(n%2);
+	}
+	else if(l==mid && r==mid)
+	{
+		return (n%2);
+	}
+}
+int main()
+{
+	ll n,l,r;
+	cin >> n >> l >> r;
+	calcPower();
+	cout << noOfOnes(n,l,r);
+	return 0;
+}
